7d791c422bb5134cd55f1f3e787b36efc47b4016
morsematters <ericamorse@ericamorse-macbookpro2.roam.corp.google.com>
2014-06-21 18:10:56 -0700

    Initial Commit

---
+++ facebookPost/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  facebookPost
+//
+//  Created by Erica Morse on 6/21/14.
+//  Copyright (c) 2014 YouTube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ facebookPostTests/facebookPostTests.m
@@ -0,0 +1,34 @@
+//
+//  facebookPostTests.m
+//  facebookPostTests
+//
+//  Created by Erica Morse on 6/21/14.
+//  Copyright (c) 2014 YouTube. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface facebookPostTests : XCTestCase
+
+@end
+
+@implementation facebookPostTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
